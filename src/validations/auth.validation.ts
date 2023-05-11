3b653c1b1ce2c4d567b572928451997c30cbccfc
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-11 13:57:27 -0400

    Fix empty string validation error message for /auth/register route by adding 'string.empty' key to messages.

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -2,18 +2,22 @@ import Joi from 'joi';
 
 export const register = Joi.object({
   fullName: Joi.string().required().messages({
-    'any.required': 'Enter your full name'
+    'any.required': 'Enter your full name',
+    'string.empty': 'Enter your full name'
   }),
   email: Joi.string().required().email().messages({
     'any.required': 'Enter your email address',
+    'string.empty': 'Enter your email address',
     'string.email': 'Email address is not valid'
   }),
   password: Joi.string().required().min(8).strict().messages({
     'any.required': 'Enter a password',
+    'string.empty': 'Enter a password',
     'string.min': 'Password length must be at least 8 characters long'
   }),
   repeatPassword: Joi.string().valid(Joi.ref('password')).required().strict().messages({
     'any.required': 'Confirm your password',
+    'string.empty': 'Confirm your password',
     'any.only': 'Passwords don\'t match'
   })
 }).prefs({
