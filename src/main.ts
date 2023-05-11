2fe8ff2bd31ac60305d521c6fefde460594a6d80
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-11 14:01:06 -0400

    Add support for json and formdata to request's body.

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import routes from './routes';
 hbs.registerPartials(path.resolve(__dirname, '../src/views/partials'));
 
 const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '../build/public')));
 app.set('views', path.join(__dirname, '../src/views'));
