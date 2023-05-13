16ac4f1547c5ecd781b99d98aa243de1a25b7cb5
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-13 15:09:12 -0400

    Create loginUserWithEmailAndPassword auth service, which gets user's email with getUserByEmail and isPasswordMatch functions.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,16 +1,31 @@
 import { PrismaClient } from "@prisma/client";
-import { RegisterReqBody } from "../controllers/types";
+import { LoginReqBody, RegisterReqBody } from "../controllers/types";
+import { getUserByEmail } from "./user.service";
+import { AuthError } from "../utils/AuthError";
+import { isPasswordMatch } from "../utils/password";
 
 const prisma = new PrismaClient();
+
+export async function loginUserWithEmailAndPassword(body: LoginReqBody) {
+  const user = await getUserByEmail(body.email);
+  if(!user || !(await isPasswordMatch(body.password, user.password))) {
+    throw new AuthError(401, 'Incorrect email or password', {
+      email: body.email,
+      password: body.password
+    });
+  }
+
+  return {
+    id: user.id,
+    email: user.email,
+    fullName: user.name
+  };
+}
 
 export async function createNewUser(body: RegisterReqBody) {
   try {
     await prisma.$connect();
-    const {
-      email,
-      id,
-      name
-    } = await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
         name: body.fullName,
         email: body.email,
@@ -18,7 +33,11 @@ export async function createNewUser(body: RegisterReqBody) {
       }
     });
 
-    return { id, name, email }
+    return {
+      id: user.id,
+      email: user.email,
+      fullName: user.name
+    };
   } catch(err) {
     console.log(err);
   } finally {
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
-export { createNewUser } from './auth.service';
+export { createNewUser, loginUserWithEmailAndPassword } from './auth.service';
 export { generateAuthToken } from './token.service';
+export { getUserByEmail } from './user.service';
