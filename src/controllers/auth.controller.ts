27a319c936b21ed60ed4395bf9db6face816b989
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-13 09:27:35 -0400

    Set newly generated jwt token to the value of Set-Cookie header, and then redirect to '/' route after registering new user.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
-import { createNewUser } from "../services";
+import { createNewUser, generateAuthToken } from "../services";
 import { RegisterReqBody } from "./types";
 
 export async function register(req: Request<{}, {}, RegisterReqBody>, res: Response) {
-  await createNewUser(req.body);
+  const user = await createNewUser(req.body);
+  const token = await generateAuthToken(user);
+  const minutes = process.env.JWT_ACCESS_TOKEN_EXPIRATION_MINUTES;
+  if(!minutes) {
+    throw new Error('JWT_ACCESS_TOKEN_EXPIRATION_MINUTES env variable undefined');
+  }
+  res.header('Set-Cookie', [
+    `blat=${token?.access.token}; Path=/; Max-Age=${+minutes * 60}; SameSite=Strict; Secure`
+  ]);
+  res.redirect('/');
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,2 @@
 export { createNewUser } from './auth.service';
+export { generateAuthToken } from './token.service';
