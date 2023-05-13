22171fa729a585dab12646ed08c340972d1ab48d
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-13 09:48:55 -0400

    Rename atuh route express Router instance variable to router.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,9 +3,9 @@ import { register } from '../controllers';
 import { validateRegister } from '../middleware/validate';
 import { authRouteErrorHandler } from '../middleware/error';
 
-const app: express.Router = express.Router();
+const router: express.Router = express.Router();
 
-app.post('/register', validateRegister, register);
-app.use(authRouteErrorHandler);
+router.post('/register', validateRegister, register);
+router.use(authRouteErrorHandler);
 
-export default app;
+export default router;
