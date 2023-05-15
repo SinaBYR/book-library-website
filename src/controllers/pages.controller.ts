de1e18293af8add964a866e8ce7bdcede945b86a
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-15 09:45:00 -0400

    Update pages controller to support logged in and not logged in user views.

--- src/controllers/pages.controller.ts
+++ src/controllers/pages.controller.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from "express";
 
 export function renderSignUpPage(req: Request, res: Response) {
+  if(req.user) {
+    return res.redirect('/');
+  }
+
   res.render('pages/signup/page');
 }
 
 export function renderSignInPage(req: Request, res: Response) {
+  if(req.user) {
+    return res.redirect('/');
+  }
+
   res.render('pages/signin/page');
 }
 
 export function renderHomePage(req: Request, res: Response) {
-  res.render('pages/home/page');
+  res.render('pages/home/page', {
+    user: req.user
+  });
 }
