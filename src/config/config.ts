440d302129c559e6abc077352e79a2fc16121bd1
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-15 10:39:04 -0400

    Remove checking minutes variable in register auth controller and move it to config.ts file, and use catchAsync wrapper.

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ export default {
   jwt: {
     secret: process.env.JWT_SECRET,
     accessTokenExpMinutes: +process.env.JWT_ACCESS_TOKEN_EXPIRATION_MINUTES!,
-    accessSessionExpMinutes: process.env.JWT_ACCESS_SESSION_EXPIRATION_MINUTES
+    accessSessionExpMinutes: +process.env.JWT_ACCESS_SESSION_EXPIRATION_MINUTES!
   },
   port: +process.env.PORT!
 }
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,28 +1,26 @@
 import { Request, Response } from "express";
-import { createNewUser, generateAuthToken, loginUserWithEmailAndPassword } from "../services";
+import { registerUserWithEmailAndPassword, generateAuthToken, loginUserWithEmailAndPassword } from "../services";
 import { RegisterReqBody, LoginReqBody } from "./types";
 import catchAsync from "../utils/catchAsync";
+import config from "../config/config";
 
 export const login = catchAsync(async (req: Request<{}, {}, LoginReqBody>, res: Response) => {
   const user = await loginUserWithEmailAndPassword(req.body);
   const token = await generateAuthToken(user);
-  const minutes = process.env.JWT_ACCESS_TOKEN_EXPIRATION_MINUTES;
+  const minutes = config.jwt.accessSessionExpMinutes;
   res.header('Set-Cookie', [
-    `blat=${token?.access.token}; Path=/; Max-Age=${+minutes! * 60}; SameSite=Strict; Secure`
+    `blat=${token?.access.token}; Path=/; Max-Age=${minutes * 60}; SameSite=Strict; Secure`
   ]);
 
   res.redirect('/');
 })
 
-export async function register(req: Request<{}, {}, RegisterReqBody>, res: Response) {
-  const user = await createNewUser(req.body);
+export const register = catchAsync(async (req: Request<{}, {}, RegisterReqBody>, res: Response) => {
+  const user = await registerUserWithEmailAndPassword(req.body);
   const token = await generateAuthToken(user);
-  const minutes = process.env.JWT_ACCESS_TOKEN_EXPIRATION_MINUTES;
-  if(!minutes) {
-    throw new Error('JWT_ACCESS_TOKEN_EXPIRATION_MINUTES env variable undefined');
-  }
+  const minutes = config.jwt.accessSessionExpMinutes;
   res.header('Set-Cookie', [
-    `blat=${token?.access.token}; Path=/; Max-Age=${+minutes * 60}; SameSite=Strict; Secure`
+    `blat=${token?.access.token}; Path=/; Max-Age=${minutes * 60}; SameSite=Strict; Secure`
   ]);
   res.redirect('/');
-}
+})
