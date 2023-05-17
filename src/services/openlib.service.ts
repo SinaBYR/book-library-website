58c4af27c03fdcee21c6cb1fb14956ebfd4222f2
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-17 12:40:58 -0400

    Fetch only 5 records of trending books on homepage.

--- src/services/openlib.service.ts
+++ src/services/openlib.service.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import { HttpResponseError } from '../utils/errors';
 
 export async function getTrendingBooks() {
-  const response = await fetch('https://openlibrary.org/trending/now.json');
+  const response = await fetch('https://openlibrary.org/trending/now.json?limit=5');
   if(!response.ok) {
     throw new HttpResponseError(response.status, response.statusText, response);
   }
