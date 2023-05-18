90aebdf664fd1637122f20b829b9a0ac08a6aaa4
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-05-18 04:18:16 -0400

    Add getTopSubjects openlib service.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 export { registerUserWithEmailAndPassword, loginUserWithEmailAndPassword, logoutUser, logoutAllExceptForCurrent } from './auth.service';
 export { generateAuthToken, verifyToken, deleteToken, deleteAllTokensExceptForCurrent } from './token.service';
 export { createNewUser, isEmailTaken, getUserByEmail } from './user.service';
-export { getTrendingBooks } from './openlib.service';
+export { getTrendingBooks, getTopSubjects } from './openlib.service';
--- src/services/openlib.service.ts
+++ src/services/openlib.service.ts
@@ -1,12 +1,26 @@
 import fetch from 'node-fetch';
 import { HttpResponseError } from '../utils/errors';
 
-export async function getTrendingBooks() {
-  const response = await fetch('https://openlibrary.org/trending/now.json?limit=5');
+export async function getTopSubjects() {
+  const response = await fetch('https://openlibrary.org/search/subjects.json?q=%2A&limit=10');
+
   if(!response.ok) {
     throw new HttpResponseError(response.status, response.statusText, response);
   }
 
-  const books = response.json();
+  const subjects = await response.json();
+
+  return subjects;
+}
+
+export async function getTrendingBooks() {
+  const response = await fetch('https://openlibrary.org/trending/now.json?limit=10');
+
+  if(!response.ok) {
+    throw new HttpResponseError(response.status, response.statusText, response);
+  }
+
+  const books = await response.json();
+
   return books;
 }
