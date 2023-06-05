d65451dc484b989b7e8a0015f3fd5cbc0abb0267
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-06-05 13:28:03 -0400

    Add expansion/collapse to subjects inside work page.

--- src/views/pages/work/index.ts
+++ src/views/pages/work/index.ts
@@ -1,28 +1,56 @@
 const bioText = document.querySelector('.bio-text') as HTMLDivElement;
-const expanderFade = document.querySelector('.bio-text-expander-fade') as HTMLDivElement;
-const expanderBtn = document.querySelector('.bio-text-expander-button')! as HTMLAnchorElement;
+const bioTextExpanderFade = document.querySelector('.bio-text-expander-fade') as HTMLDivElement;
+const bioTextExpanderBtn = document.querySelector('.bio-text-expander-button')! as HTMLAnchorElement;
+const subjects = document.querySelector('.subjects') as HTMLDivElement;
+const subjectsExpanderFade = document.querySelector('.subjects-expander-fade') as HTMLDivElement;
+const subjectsExpanderBtn = document.querySelector('.subjects-expander-button')! as HTMLAnchorElement;
 
 window.addEventListener('load', () => {
   const paragraph = bioText.querySelector('p') as HTMLParagraphElement;
+
   if(+paragraph.clientHeight < 90) {
-    expanderFade.style.display = 'none';
+    bioTextExpanderFade.style.display = 'none';
     bioText.style.height = 'auto';
-    expanderBtn.style.display = 'none';
+    bioTextExpanderBtn.style.display = 'none';
+  }
+
+	const subjectsWrapper = subjects.querySelector('.subjects-wrapper') as HTMLDivElement;
+	
+	if(+subjectsWrapper.clientHeight < 90) {
+    subjectsExpanderFade.style.display = 'none';
+    subjectsWrapper.style.height = 'auto';
+    subjectsExpanderBtn.style.display = 'none';
   }
 });
 
-expanderBtn.addEventListener('click', e => {
+subjectsExpanderBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  if(subjects.classList.contains('is-expanded')) {
+    subjects.classList.remove('is-expanded');
+    subjects.style.height = '100px';
+    subjectsExpanderFade.style.display = 'block';
+    subjectsExpanderBtn.innerHTML = '<i class="fa fa-angle-down"></i> <span>Expand</span>';
+  } else {
+    subjects.classList.add('is-expanded');
+    subjects.style.height = 'auto';
+    subjectsExpanderFade.style.display = 'none';
+    subjectsExpanderBtn.innerHTML = '<i class="fa fa-angle-up"></i> <span>Collapse</span>';
+  }
+});
+
+bioTextExpanderBtn.addEventListener('click', e => {
   e.preventDefault();
 
   if(bioText.classList.contains('is-expanded')) {
     bioText.classList.remove('is-expanded');
     bioText.style.height = '100px';
-    expanderFade.style.display = 'block';
-    expanderBtn.innerHTML = '<i class="fa fa-angle-down"></i> <span>Read more</span>';
+    bioTextExpanderFade.style.display = 'block';
+    bioTextExpanderBtn.innerHTML = '<i class="fa fa-angle-down"></i> <span>Read more</span>';
   } else {
     bioText.classList.add('is-expanded');
     bioText.style.height = 'auto';
-    expanderFade.style.display = 'none';
-    expanderBtn.innerHTML = '<i class="fa fa-angle-up"></i> <span>Read less</span>';
+    bioTextExpanderFade.style.display = 'none';
+    bioTextExpanderBtn.innerHTML = '<i class="fa fa-angle-up"></i> <span>Read less</span>';
   }
 });
